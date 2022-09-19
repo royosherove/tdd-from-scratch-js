9e3120962fa7d40337310aead3021396aea6a847
royo <royo>
2022-09-20 02:06:16 +0300

    fix

--- tests/string-calculator.test.ts
+++ tests/string-calculator.test.ts
@@ -3,7 +3,7 @@ import {calculate} from '../src/calculate-with-log'
 describe('string calculator', () => { 
     describe('add', () => { 
         test('with empty input, returns zero',()=>{
-        }
+        })
      })
 
  })
