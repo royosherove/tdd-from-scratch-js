7969464cb605a11797ed02ca46367571a01aa8e6
royo <royo>
2022-09-20 01:51:18 +0300

    sample

--- calculate-with-log.spec.ts
+++ calculate-with-log.spec.ts
@@ -1,10 +1,7 @@
 import {calculate} from './calculate-with-log'
 
-describe('calculate1', () => { 
+describe('calculate', () => { 
     it('calls the logger every time',()=>{
-        let logged;
-        calculate(1,2,(text)=>logged = text);
-        expect(logged).toBe(3);
     })
 
  })
