f065a7dda90ba27da12ae4a4ed7f6039f8f15c6b
royo <royo>
2022-09-20 02:02:50 +0300

    string calc initial test

---
+++ tests/string-calculator.test.ts
@@ -0,0 +1,9 @@
+import {calculate} from '../src/calculate-with-log'
+
+describe('string calculator', () => { 
+    describe('add', () => { 
+        test('with empty input, returns zero',()=>{
+        }
+     })
+
+ })
